7f698ab0e35e6841d25bf0794056b6deee5b2a09
haah21 <55376327+haah21@users.noreply.github.com>
2024-12-02 19:58:33 +0900

    java class 도입

---
+++ src/class1/ClassStart1.java
@@ -0,0 +1,25 @@
+package class1;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class ClassStart1 {
+    public static void main(String[] args){
+        String studentName = "학생1";
+        int student1Age = 15;
+        int student1Grade = 90;
+
+        String studentName2 = "학생2";
+        int student2Age = 16;
+        int student2Grade = 80;
+
+        String studentName3 = "학생3";
+        int student3Age =  17;
+        int student3Grade = 80;
+
+        System.out.println("이름: " + studentName + " 나이 : " + student1Age + " 성적 : "+ student1Grade);
+        System.out.println("이름: " + studentName2 + " 나이 : " + student2Age + " 성적 : "+ student2Grade);
+        System.out.println("이름: " + studentName3 + " 나이 : " + student3Age + " 성적 : "+ student3Grade);
+
+
+    }
+}
